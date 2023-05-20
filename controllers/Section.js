ca74ba753c8323c5746dd546061adb972ceb6d79
Help Desk for FZ_fshelpS Software <z_fshelp@nrl.co.in>
2023-05-20 11:29:09 +0530

    Section Controller updatesection  handle function Done

--- controllers/Section.js
+++ controllers/Section.js
@@ -37,4 +37,33 @@ exports.createSection = async(req, res) => {
             error:error.message,
         });
     }
-} 
+};
+
+exports.updateSection = async (req, res) => {
+    try {
+        //data input
+        const {sectionName, sectionId} = req.body
+        //validation
+        if(!sectionName|| ! sectionId){
+            return res.status(400).json({
+                success:false,
+                message:"Missing Properties",
+            });
+        }
+        //update data by ID
+        const UpdateSection = await Section.findByIdAndUpdate(sectionId,{sectionName},{new:true});
+        //return  response
+        return res.status(200).json({
+            success:true,
+            message:"Section Updated",
+            UpdateSection,
+        });
+        
+    } catch (error) {
+        return res.status(500).json({
+            success:true,
+            message:"Unable to Update Section, Please try again ",
+            error:error.message,
+        });
+    }
+}
