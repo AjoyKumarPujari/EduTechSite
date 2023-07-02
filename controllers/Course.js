599e5af34d5bb0f5326ece1fc0dffa103b4bf0c2
Ajoy Kumar Pujari <ajoykumarpujari@gmail.com>
2023-07-02 13:53:16 +0530

    create course api tested and done

--- controllers/Course.js
+++ controllers/Course.js
@@ -3,24 +3,61 @@ const  Course = require("../models/Course");
 const Category = require("../models/Category");
 const User = require("../models/User");
 const {uploadImageToCloudinary} = require("../utils/imageUploader");
+
 //create course
 exports.createCourse = async (req, res)=>{
     try {
-        // data fetch 
-        const {courseName, courseDescription, whatYouWillLearn, price, tag} = req.body;
-        //get thumbnail
+        //get usr
+        const userId = req.user.id; 
         const thumbnail = req.files.thumbnailImage;
+        let {
+            courseName, 
+            courseDescription, 
+            whatYouWillLearn, 
+            price, 
+            tag,
+            category,
+            status,
+            instructions,
+        } = req.body;
+
+        console.log(courseName);
+        console.log(courseDescription);
+        console.log(whatYouWillLearn);
+        console.log(price);
+        console.log(tag);
+        console.log(category);
+        console.log(status);
+        console.log(instructions);
+        //get thumbnail
+        
+      // console.log(thumbnail);
         //validation for fetch data
-        if(!courseName || !courseDescription || !whatYouWillLearn || !price || !tag){
+        if(
+            !courseName || 
+            !courseDescription || 
+            !whatYouWillLearn || 
+            !price || 
+            !tag ||
+            !category||
+            !thumbnail
+           
+            
+            ){
             return res.status(400).json({
                 success:false,
                 message:"All fields are Required",
             }); 
         }
+
+        if(!status || status === undefined){
+            status = "Draft";
+        }
         //validation for instructor for adding course instructor object ID
         //check instructor details
-        const userId = req.User.id;
-        const instructorDetails = await User.findById(userId);
+        const instructorDetails = await User.findById(userId,{
+            accountType: "Instructor",
+        });
         console.log(instructorDetails);
 
         //validation instructorDetails
@@ -31,47 +68,67 @@ exports.createCourse = async (req, res)=>{
             }); 
         }
         //check given tag is valid or not
-        const tagDetails = await Tag.findById(tag);
-        if(!tagDetails){
+        const categorydetails  = await Category.findById(category);
+        if(!categorydetails){
             return res.status(404).json({
                 success:false,
-                message:"TagDetails Not found",
+                message:"categorydetails Not found",
             });
         }
         //upload Image ti cloyudinary
-        const thumbnailImage = await uploadImageToCloudinary(thumbnail, process.env.FOLDER_NAME);
-
+        const thumbnailImage = await uploadImageToCloudinary(
+            thumbnail, 
+            process.env.FOLDER_NAME,
+            1000,
+            1000
+            )
+       
         //Create an Entry for new course
         const newCourse = await Course.create({
             courseName,
             courseDescription,
             instructor: instructorDetails._id,
             whatYouWillLearn: whatYouWillLearn,
-            tag: tagDetails._id,
+            tag: tag,
+            category: categorydetails._id,
             thumbnail: thumbnailImage.secure_url,
+            status: status,
+			instructions: instructions,
 
         })
         //add the new course to the user schema of instructor
         await User.findByIdAndUpdate(
-            {_id:instructorDetails._id},
+            {
+                _id:instructorDetails._id
+            },
                 {
                     $push: {
                         courses: newCourse._id,
-                    }
+                        
+                    },
                 },
-                {new:true},
+                {new:true}
             );
 
         //update the tag Schema
-        
+        // Add the new course to the Categories
+		await Category.findByIdAndUpdate(
+			{ _id: category },
+			{
+				$push: {
+					course: newCourse._id,
+				},
+			},
+			{ new: true }
+		);
         //return response
         return res.status(200).json({
             success:true,
             message:"Course Created Successfully",
             data:newCourse
-        });;
+        });
     } catch (error) {
-        console.log(error);
+        console.error(error);
         return res.status(500).json({
             success:false,
             message:"Faile to create Course",
--- models/Course.js
+++ models/Course.js
@@ -1,53 +1,59 @@
 const mongoose = require("mongoose");
-const courseSchema =  new mongoose.Schema({
-    courseName:{
-        type:String,
-        require:true,
-    },
-    courseDescription:{
-        type:String,
-        require:true,
-    },
-    instructor:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User",
-        require:true,
-    },
-    whatYouWillLearn:{
-        type:String,
-        require:true,
-    },
-    courseContent:[
-        {
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"Section",
-        }
-    ],
-    ratingAndReviews: [
-        {
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"RatingsAndReview",
-        }
-    ],
-    price:{
-        type:String,
-        require:true,
-    },
-    thumbnail:{
-        type:String,
-        require:true,
-    },
-    tag:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Tag",
-    },
-    studentsEnrolled:[
-        {
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"User",
-            require:true,
-        }
-    ]
 
+// Define the Courses schema
+const coursesSchema = new mongoose.Schema({
+	courseName: { type: String },
+	courseDescription: { type: String },
+	instructor: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: true,
+		ref: "user",
+	},
+	whatYouWillLearn: {
+		type: String,
+	},
+	courseContent: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Section",
+		},
+	],
+	ratingAndReviews: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "RatingAndReview",
+		},
+	],
+	price: {
+		type: Number,
+	},
+	thumbnail: {
+		type: String,
+	},
+	tag: {
+		type: [String],
+		required: true,
+	},
+	category: {
+		type: mongoose.Schema.Types.ObjectId,
+		// required: true,
+		ref: "Category",
+	},
+	studentsEnrolled: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			required: true,
+			ref: "user",
+		},
+	],
+	instructions: {
+		type: [String],
+	},
+	status: {
+		type: String,
+		enum: ["Draft", "Published"],
+	},
 });
-module.exports = mongoose.model("Course",courseSchema);
+
+// Export the Courses model
+module.exports = mongoose.model("Course", coursesSchema);
